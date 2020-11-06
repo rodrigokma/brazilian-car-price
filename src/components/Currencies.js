dbe0770ddb526d72bc967fc77f824a9937c56163
Rodrigo Campos <rodrigo.ccm@live.com>
2020-11-06 18:31:57 -0300

    Component Currencies.js added for future functionality. Style.css added for styling.

---
+++ src/components/Currencies.js
@@ -0,0 +1,11 @@
+
+
+export const usd = 'U$';
+export const eur = '€';
+export const gbp = '£';
+export const jpy = '¥';
+export const aud = 'US $';
+export const chf = 'Fr';
+export const cad = 'C$';
+export const cny = '元';
+
